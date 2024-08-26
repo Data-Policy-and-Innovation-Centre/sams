eb00d6dc4bd3489c9ee9663ce98a4a6a0104e8ef
Yashaswi Mohanty <josh.mohanty@gmail.com>
2024-08-26 16:48:41 +0530

    Add metadata constants to config.py
    1) Create path directory if does not exist
    2) Add available year range data for SAMS
    3) Halt execution if API auth not present

--- sams/config.py
+++ sams/config.py
@@ -20,15 +20,44 @@ EXTERNAL_DATA_DIR = DATA_DIR / "external"
 OUTPUT_DIR = PROJ_ROOT / "output"
 FIGURES_DIR = OUTPUT_DIR / "figures"
 
+# Verify that all the directories exist
+for path in [DATA_DIR, RAW_DATA_DIR, INTERIM_DATA_DIR, PROCESSED_DATA_DIR, EXTERNAL_DATA_DIR, OUTPUT_DIR, FIGURES_DIR]:
+    if not path.exists():
+        logger.info(f"Creating directory {path}")
+        path.mkdir(parents=True, exist_ok=True)
+
 # Auth
 API_AUTH = os.path.join(PROJ_ROOT, "credentials.json")
+if not Path(API_AUTH).exists():
+    logger.critical(f"ERROR: {API_AUTH} not found. This file is required for API authentication.")
+    exit(1)
 
-# If tqdm is installed, configure loguru with tqdm.write
-# https://github.com/Delgan/loguru/issues/135
+# Error constants
+ERRMAX = 10
+
+# Metadata
+STUDENT = {
+   "PDIS":{"yearmin":2020,"yearmax":2024},
+   "ITI":{"yearmin":2017,"yearmax":2024},
+   "Diploma":{"yearmin":2018,"yearmax":2024}
+}
+
+
+INSTITUTE = {
+   "PDIS":{"yearmin":2020,"yearmax":2024},
+   "ITI":{"yearmin":2017,"yearmax":2024},
+   "Diploma":{"yearmin":2018,"yearmax":2024}
+}
+
+SOF = {
+    "tostring": {1:"Govt", 5:"Pvt"},
+    "toint":{"Govt":1,"Pvt":5}
+}
+
 try:
     from tqdm import tqdm
 
     logger.remove(0)
     logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
 except ModuleNotFoundError:
-    pass
+    logger.warning("Module tqdm not found")
