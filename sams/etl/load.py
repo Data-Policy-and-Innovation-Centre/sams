9ec7d98a956fbb29c4b272d45523e6223e7661b4
Yashaswi Mohanty <josh.mohanty@gmail.com>
2024-08-27 15:27:01 +0530

    Initial commit to describe ORM for data

--- sams/etl/load.py
+++ sams/etl/load.py
@@ -1,47 +1,52 @@
-from sqlalchemy import create_engine, Column, Integer, String, Table, MetaData
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sams.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+from concurrent.futures import ThreadPoolExecutor
+
+Base = declarative_base()
+
+class Student(Base):
+    __tablename__ = 'students'
+    id = Column(Integer, primary_key=True)
+    module = Column(String)
+    year = Column(Integer)
+    source_of_fund = Column(Integer)
+    # Add other columns as needed
+
+class Institute(Base):
+    __tablename__ = 'institutes'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    address = Column(String)
+    # Add other columns as needed
 
 class SamsDataLoader:
-    def __init__(self, db_url):
+    def __init__(self, db_url, downloader):
         self.engine = create_engine(db_url)
-        self.session = sessionmaker(bind=self.engine)()
-        self.metadata = MetaData()
+        Base.metadata.create_all(self.engine)
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
+        self.downloader = downloader 
 
-    def define_tables(self):
-        self.students_table = Table('students', self.metadata,
-            Column('id', Integer, primary_key=True),
-            Column('name', String),
-            Column('program', String),
-            Column('year', Integer),
-            # Add other relevant columns here
-        )
+    def load_student_data(self, student_data):
+        for data in student_data:
+            student = Student(
+                module=data['module'],
+                year=data['year'],
+                source_of_fund=data['source_of_fund'],
+                # Add other columns as needed
+            )
+            self.session.add(student)
+        self.session.commit()
 
-        self.institutes_table = Table('institutes', self.metadata,
-            Column('id', Integer, primary_key=True),
-            Column('name', String),
-            Column('program', String),
-            Column('year', Integer),
-            # Add other relevant columns here
-        )
-
-        # Create the tables in the database
-        self.metadata.create_all(self.engine)
-
-    def load_data(self, data, table):
-        for year, programs in data.items():
-            for program, entries in programs.items():
-                for entry in entries:
-                    insert_stmt = table.insert().values(
-                        id=entry['id'],
-                        name=entry['name'],
-                        program=program,
-                        year=year,
-                        # Add other relevant fields here
-                    )
-                    self.session.execute(insert_stmt)
-
-    def commit(self):
+    def load_institute_data(self, institute_data):
+        for data in institute_data:
+            institute = Institute(
+                name=data['name'],
+                address=data['address'],
+                # Add other columns as needed
+            )
+            self.session.add(institute)
         self.session.commit()
 
     def close(self):
