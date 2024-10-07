85837fc304440de8ef9353ee8256e89b3a7e8349
Yashaswi Mohanty <josh.mohanty@gmail.com>
2024-10-08 00:54:19 +0530

    Update build_dataset.py

--- scripts/build_dataset.py
+++ scripts/build_dataset.py
@@ -1,9 +1,9 @@
 from sams.etl.orchestrate import SAMSDataOrchestrator
-from sams.config import API_AUTH
+from sams.config import PROJ_ROOT
 from pathlib import Path
 
-if not Path(API_AUTH).exists():
-    raise FileNotFoundError(f"API_AUTH file not found at {API_AUTH}")
+if not Path(PROJ_ROOT / ".env").exists():
+    raise FileNotFoundError(f".env file not found at {PROJ_ROOT}/.env")
 
 orchestrator = SAMSDataOrchestrator()    
 orchestrator.process_data("students")
