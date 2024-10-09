8def01983426d4825d63668c8722855ed54dc4d6
Yashaswi Mohanty <josh.mohanty@gmail.com>
2024-10-09 22:11:17 +0530

    Auto detect which modules/years to download
    Check the db (if it exists) for the total number of records for each (module, year) and compare that with expected records for the same (module, year). If the two figures match, do not download the records for that (module,year) during the "build_dataset" routine

--- sams/util.py
+++ sams/util.py
@@ -1,5 +1,9 @@
 from datetime import datetime
 from loguru import logger
+from sams.config import RAW_DATA_DIR, LOGS
+import os
+import sqlite3
+import pandas as pd
 
 def is_valid_date(date_string):
     formats = [
@@ -19,4 +23,39 @@ def is_valid_date(date_string):
         except ValueError:
             continue  # Try the next format
     
-    return False, None  # No formats matched, date is invalid    
+    return False, None  # No formats matched, date is invalid   
+
+def get_existing_modules(table_name: str = "students") -> list:
+
+    if table_name not in ["students", "institutes"]:
+        raise ValueError(f"Invalid table name: {table_name}")
+    
+    db_path = os.path.join(RAW_DATA_DIR, "sams.db")
+    counts_path = os.path.join(LOGS, table_name + "_count.csv")
+
+    if not os.path.exists(db_path):
+        return []
+    
+    if not os.path.exists(counts_path):
+        return []
+    
+    counts = pd.read_csv(counts_path)
+    
+    if table_name == "students":
+        query = """SELECT module, academic_year, COUNT(*) AS num_observations 
+                    FROM students 
+                    GROUP BY module, academic_year"""
+        with sqlite3.connect(db_path) as conn:
+            cur = conn.cursor()
+            cur.execute(query)
+            existing_modules = cur.fetchall()
+
+        existing_modules = [list(row) for row in existing_modules]
+        existing_modules = [(module, year) for module, year, num_obs in existing_modules 
+                            if num_obs == counts[(counts["module"] == module) & (counts["academic_year"] == year)]["count"].values[0]]
+        return existing_modules
+    
+    else:
+        return []
+
+    
