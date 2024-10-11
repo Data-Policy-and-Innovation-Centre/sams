6a473b0a0a5757bee58330874b90d587cfce9a7e
Yashaswi Mohanty <josh.mohanty@gmail.com>
2024-10-11 18:01:15 +0530

    Update paths.R

--- notebooks/paths.R
+++ notebooks/paths.R
@@ -1,9 +1,25 @@
-if (interactive()) {
-  script_path <- getwd()  # Current working directory
-} else {
-  script_path <- normalizePath(commandArgs(trailingOnly = FALSE)[grep("--file=", commandArgs(trailingOnly = FALSE))])
-  script_path <- sub("--file=", "", script_path)
+packages <- c("tidyverse","RSQLite","rprojroot","knitr",
+              "kableExtra","here","rmarkdown")
+
+# Loop through the package names and install if not already installed
+for (pkg in packages) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg)
+  }
 }
-root <- dirname(script_path)
+
+
+library(here)
+
+# Load paths
+root <- dirname(dirname(here::here("paths.R")))
 data <- file.path(root,"data")
 raw_data <- file.path(data,"raw")
+
+# Load figure constants
+optfig.labfontsize = 20 + 8
+optfig.axfontsize = 18 + 8
+optfig.legfontsize = 16
+optfig.ticklength = -.2
+optfig.font = 'Times'
+optfig.axisweight = 1
