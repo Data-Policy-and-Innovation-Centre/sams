a7a2083491765b3b33c266815dd789b095a80d8f
Yashaswi Mohanty <josh.mohanty@gmail.com>
2024-11-15 14:12:51 +0530

    Update config.R to follow R code conventions

--- scripts/config.R
+++ scripts/config.R
@@ -26,19 +26,20 @@ get_path <- function(name) {
 
 # Directory structure
 paths <- c()
-paths$DATA_DIR = file.path(here(),"data")
-paths$RAW_DATA_DIR = file.path(paths$DATA_DIR, "raw")
-paths$INTERIM_DATA_DIR = file.path(paths$DATA_DIR, "interim")
-paths$PROCESSED_DATA_DIR = file.path(paths$DATA_DIR, "processed")
-paths$OUTPUT_DIR = file.path(here(),"output")
-paths$TABLES_DIR = file.path(paths$OUTPUT_DIR, "tables")
-paths$FIGURES_DIR = file.path(paths$OUTPUT_DIR, "figures")
+paths$DATA_DIR <- file.path(here(),"data")
+paths$RAW_DATA_DIR <- file.path(paths$DATA_DIR, "raw")
+paths$INTERIM_DATA_DIR <- file.path(paths$DATA_DIR, "interim")
+paths$PROCESSED_DATA_DIR <- file.path(paths$DATA_DIR, "processed")
+paths$OUTPUT_DIR <- file.path(here(),"output")
+paths$TABLES_DIR <- file.path(paths$OUTPUT_DIR, "tables")
+paths$FIGURES_DIR <- file.path(paths$OUTPUT_DIR, "figures")
 
 
 # Load figure constants
-optfig.labfontsize = 20 + 8
-optfig.axfontsize = 18 + 8
-optfig.legfontsize = 16
-optfig.ticklength = -.2
-optfig.font = 'Times'
-optfig.axisweight = 1
+optfig <- c()
+optfig$labfontsize <- 20 + 8
+optfig$axfontsize <- 18 + 8
+optfig$legfontsize <- 16
+optfig$ticklength <- -.2
+optfig$font <- "Times"
+optfig$axisweight <- 1
