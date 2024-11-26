f8ade9d0e1b57d30e1415cb9902bf6372d79ddd1
Yashaswi Mohanty <josh.mohanty@gmail.com>
2024-11-26 13:57:40 +0530

    Build new draft of report

--- scripts/build_reports.R
+++ scripts/build_reports.R
@@ -9,12 +9,7 @@ library("here")
 here::i_am("README.md")
 source(here("scripts/config.R"))
 
-# Nov 2024 -- PPT
-rmarkdown::render(here("notebooks/4.05-ym-nov2024-presentation.Rmd"), 
-    output_file = here("output/ppt-nov2024.pptx"))
-
-
 # Nov 2024 -- Report
-rmarkdown::render(here("notebooks/4.04-ym-nov2024-report.Rmd"), 
+rmarkdown::render(here("notebooks/4.06-ym-nov2024-report.Rmd"), 
     output_file = here("output/report-nov2024.docx"))
 
