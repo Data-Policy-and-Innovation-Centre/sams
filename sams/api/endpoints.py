2b965cc8ed5549f270bacaa7316299d3b206e190
sharma28720 <ssharma28720@uchicagotrust.org>
2025-08-11 13:13:15 +0530

    api_endpoint_script: Add higher education endpoint to endpoints.py

--- sams/api/endpoints.py
+++ sams/api/endpoints.py
@@ -6,3 +6,9 @@ class Endpoints:
 
     def get_institute_data(self):
         return f"{self.BASE_URL}GetDPICInstituteData"
+    
+    def get_plus2_student_data(self):
+        return f"{self.BASE_URL}GetHSSStudentData"
+    
+    def get_deg_student_data(self):
+        return f"{self.BASE_URL}GETDEGStudentData"
